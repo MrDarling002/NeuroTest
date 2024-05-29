a2c51380cd8997c5154f85cef8785fa9e770b7e7
Абдуллаев Санжарбек <110113899+MrDarling002@users.noreply.github.com>
2024-05-29 14:21:59 +0300

    Add files via upload

--- register-respondent.php
+++ register-respondent.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once "db_connect.php";
+require_once "db-connect.php";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['register'])) {
     $name = $_POST['name'];
